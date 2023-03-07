c22b518db5d6066792c35ac0f54c0bb6ece84cf6
ericaatann <89226691+ericaatann@users.noreply.github.com>
2023-03-07 13:26:38 -0800

    problem 5

---
+++ Book.java
@@ -0,0 +1,8 @@
+public class Book {
+    private String name;
+    private String author;
+
+    public Book(){
+        
+    }
+}
---
+++ Course.java
@@ -0,0 +1,7 @@
+public class Course{
+    private int code;
+
+    public Course(int code){
+        this.code = code;
+    }
+}
